e09dbb843f1a65add97745388a67d8803c75fa82
NGAW <ngaw.mobile.dev@gmail.com>
2026-01-24 20:34:38 +0530

    Add ip address

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -9,10 +9,9 @@ return {
       { "<c-space>", desc = "Increment selection" },
       { "<bs>", desc = "Decrement selection", mode = "x" },
     },
-    opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
+    opts = function()
+      -- Deduplicate ensure_installed list
+      local ensure_installed = {
         "bash",
         "c",
         "html",
@@ -31,30 +30,28 @@ return {
         "vim",
         "vimdoc",
         "yaml",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    },
-    config = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        ---@type table<string, boolean>
-        local added = {}
-        opts.ensure_installed = vim.tbl_filter(function(lang)
+      }
+      local added = {}
+      return {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = vim.tbl_filter(function(lang)
           if added[lang] then
             return false
           end
           added[lang] = true
           return true
-        end, opts.ensure_installed)
-      end
-      require("nvim-treesitter.configs").setup(opts)
+        end, ensure_installed),
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      }
     end,
   },
 }
